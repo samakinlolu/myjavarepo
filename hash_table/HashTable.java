d43ad934f4c6e0c06d36dc7faaacc6b029838ab0
Sam <75106131+samakinlolu@users.noreply.github.com>
2021-11-20 21:57:32 -0500

    Update HashTable.java

--- hash_table/HashTable.java
+++ hash_table/HashTable.java
@@ -1,1 +1,46 @@
+package hash_table;
+import hash_table.testdata.*;
+
+public class HashTable {
+    int tableSize; // table size
+    SLL[] table;
+
+    // default constructor, which uses default table size 3
+    public HashTable() {
+        // implement this method
+    }
+    
+    // constructor, which uses size as the table size
+    public HashTable(int size) {
+        // implement this method
+    }
+    
+    // search for item in the table
+    // if found return true
+    // else return false
+    public boolean find(String item) {
+        // implement this method
+        return false;
+    }
+    
+    // insert (item1, item2) to the table
+    // use item1 as the key
+    public void insert(String item1, String item2) {
+        //implement this method
+    }
+    
+    // delete the pair whose key value is item
+    // if deleted return true
+    // else return false 
+    public boolean erase(String item) {
+        return false;
+    }
+
+    // return the total number of nodes in the hash table    
+    public int getSize() {
+        // implement this method
+        return 0;
+    }
+    
+}
 
